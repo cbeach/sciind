203e6b74f6a23e12eec1f211096892b7479c3786
Casey Beach <mcsmash@GlaDOS.(none)>
2012-01-13 01:18:28 -0800

    initial commit.  The terminal block is just a stub

---
+++ Terminal.java
@@ -0,0 +1,16 @@
+package net.minecraft.src.sciind;
+
+import net.minecraft.src.*;
+
+public class Terminal extends Block {
+	
+	protected Terminal(int i) {
+		super(i, Material.wood);
+	}
+	
+	public Terminal(int i, int j, Material material) {
+		
+		super(i,j, material);
+	}
+
+}
